7a8c980c7ac9cc2efd6e876bd00525e0408f6f47
Marc Krejci <github@nuavant.com>
2022-03-06 20:42:31 -0800

    Remove Overlays

--- _src/index.js
+++ _src/index.js
@@ -9,8 +9,8 @@ const tobi = new Tobi()
 // FloatingPrompt({ name: 'Mobile App Landing Page', url: 'https://www.producthunt.com/posts/mobile-app-landing-page', bottom: '96px', width: '450px' })
 
 // Remove the following lines to remove the darkmode js
-import Darkmode from 'darkmode-js'
-function addDarkmodeWidget() {
-  new Darkmode().showWidget()
-}
-window.addEventListener('load', addDarkmodeWidget)
+// import Darkmode from 'darkmode-js'
+// function addDarkmodeWidget() {
+//  new Darkmode().showWidget()
+// }
+// window.addEventListener('load', addDarkmodeWidget)
